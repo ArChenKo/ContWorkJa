3a895f71cf6f227908a748b1f42884eed54dd52d
Arslan <arslan.chen@mail.ru>
2023-10-14 22:48:00 +0400

    init commit

---
+++ MainApp.java
@@ -0,0 +1,18 @@
+import Controller.Controller;
+import View.AdminView;
+import View.BaseView;
+import View.ConsumerView;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        try {
+            BaseView[] view = {new AdminView(), new ConsumerView()};
+            new Controller().appStart(view, args[0]);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("============ Лотерея игрушек ===============");
+            System.out.println("Вызов справки, введите аргумент: --help, -h");
+            System.out.println("============================================");
+        }
+    }
+}
